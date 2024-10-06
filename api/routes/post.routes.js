1af5d7632e677a70f0f5cdd3efd1607f947263ea
Akhileshait <akhileshpatidar989368@gmail.com>
2024-10-06 18:30:04 +0530

    updates commit

--- api/routes/post.routes.js
+++ api/routes/post.routes.js
@@ -3,13 +3,6 @@ import express from 'express';
 const router = express.Router();
 
 
-app.use("/api/posts", (req, res) => {
-     res.send("It Works!")
-});
-app.use("/api/posts", (req, res) => {
-     res.send("It Works!")
-});
-
 
 
 export default router;
