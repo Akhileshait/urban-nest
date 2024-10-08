913b7506c59440f4a4d427dafa5b8ff992839e10
Akhileshait <akhileshpatidar989368@gmail.com>
2024-10-08 12:09:36 +0530

    added homepage , ListPage, layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,36 @@
+import "./layout.scss"
+import "./components/navbar/Navbar.jsx"
+import Navbar from "./components/navbar/Navbar.jsx"
+import HomePage from "./routes/homePage/homePage.jsx"
+import ListPage from "./routes/listPage/listPage.jsx"
+import Layout from "./routes/layout/layout.jsx"
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <Layout/>
+      ),
+      children: [
+        { path: "/", element: <HomePage/> },
+        { path: "/list", element: <ListPage/> },
+      ],
+    },
+  ]);
+
   return (
-    <div>Hello World</div>
+    
+    <RouterProvider router={router}/>
   )
 }
 
---
+++ client/src/routes/singlePage/singlePage.jsx
@@ -0,0 +1,2 @@
+import singlePage from './singlePage.scss'
+
