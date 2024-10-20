9afbcb6e4e03b053caf10a99fa0bdd84f1b5a6b1
Akhileshait <akhileshpatidar989368@gmail.com>
2024-10-20 19:04:00 +0530

    new updates

--- api/routes/post.routes.js
+++ api/routes/post.routes.js
@@ -1,8 +1,14 @@
 import express from 'express';
+import { verifyToken } from '../middleware/verifyToken.js';
+import { addPost, deletePost, editPost, getPost, getPosts } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
-
+router.get("/", getPosts);
+router.get("/:id", getPost);
+router.post("/add", verifyToken,addPost);
+router.put("/:id", verifyToken,editPost);
+router.delete("/:id",verifyToken, deletePost);
 
 
 export default router;
