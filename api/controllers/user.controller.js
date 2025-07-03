7307792385e1fb5950866de48787b6ed737b958b
Akhilesh Patidar <akhileshpatidar989368@gmail.com>
2025-07-04 04:09:54 +0530

    added chat feature

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -140,4 +140,34 @@ const profilePosts = async (req, res) => {
   }
 };
 
-export { getUsers, getUser, updateUser, deleteUser, savePost, profilePosts };
+export const getNotificationNumber = async (req, res) => {
+  const tokenUserId = req.userId;
+  try {
+    const number = await prisma.chat.count({
+      where: {
+        userIDs: {
+          hasSome: [tokenUserId],
+        },
+        NOT: {
+          seenBy: {
+            hasSome: [tokenUserId],
+          },
+        },
+      },
+    });
+    res.status(200).json(number);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to get profile posts!" });
+  }
+};
+
+export {
+  getUsers,
+  getUser,
+  updateUser,
+  deleteUser,
+  savePost,
+  profilePosts,
+  getNotificationNumber,
+};
--- client/src/lib/loaders.js
+++ client/src/lib/loaders.js
@@ -8,17 +8,17 @@ export const singlePageLoader = async ({ request, params }) => {
 
 export const listPageLoader = async ({ request, params }) => {
   const query = request.url.split("?")[1];
-  const res = await apiRequest("/posts?" + query);
+  const postPromise = apiRequest("/posts?" + query);
   return defer({
-    postResponse: res.data,
+    postResponse: postPromise,
   });
 };
 
 export const profilePageLoader = async () => {
-  const res = await apiRequest("/users/profilePosts");
-  console.log(res);
-
+  const postPromise = apiRequest("/users/profilePosts");
+  const chatPromise = apiRequest("/chats");
   return defer({
-    postResponse: res.data,
+    postResponse: postPromise,
+    chatResponse: chatPromise,
   });
 };
